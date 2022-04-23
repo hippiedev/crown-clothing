63764058aa40be5bf4f1a4ffd357863edc3d278d
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2022-04-23 11:26:06 +0100

    add cart items

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../context/cart.context';
 import Button from '../button/button.component';
 import './product-card.styles.scss';
 
 const ProductCard = ({product}) => {
+    const {addItemToCart} = useContext(CartContext);
+    const addProductToCart = () => addItemToCart(product);
     const {name, price, imageUrl} = product;
   return (
     <div className="product-card-container">
@@ -10,7 +15,7 @@ const ProductCard = ({product}) => {
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType="inverted">Add to cart</Button>
+        <Button buttonType="inverted" onClick={addProductToCart}>Add to cart</Button>
     </div>
   )
 }
