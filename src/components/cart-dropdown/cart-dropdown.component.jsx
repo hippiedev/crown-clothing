79ff5643d54546b1f33440aa7fbc2efd00aceb27
Jonathan Agusa <54756572+hippiedev@users.noreply.github.com>
2022-04-23 19:31:12 +0100

    create checkout

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { CartContext } from "../../context/cart.context";
 import Button from "../button/button.component";
@@ -6,17 +7,18 @@ import CartItem from "../cart-item/cart-item.component";
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+    let navigate = useNavigate();
     const {cartItems} = useContext(CartContext);
   return (
-    <div  className="cart-dropdown-container">
-        <div className="cart-items">
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
         {cartItems.map((item) => (
-            <CartItem key={item.id} cartItem={item} />
+          <CartItem key={item.id} cartItem={item} />
         ))}
-        </div>
-        <Button> Go to checkout</Button>
+      </div>
+        <Button onClick={() => navigate('/checkout', {replace: true})}> Go to checkout</Button>
     </div>
-  )
+  );
 }
 
 export default CartDropdown
